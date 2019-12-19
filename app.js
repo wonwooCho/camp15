74d43039c9e535a0011d7d9541f7c8ef837392ec
wonwooCho <jwwjjang@gmail.com>
2019-12-19 21:53:58 +0900

    install sequelize, mysql2, morgan / add model.

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// db 관련
+const db = require('./models');
+
+// DB authentication
+db.sequelize.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+    return db.sequelize.sync();
+})
+.then(() => {
+    console.log('DB Sync complete.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
 const admin = require('./routes/admin');
 
 const app = express();
@@ -9,6 +28,11 @@ nunjucks.configure('template', {
     autoescape: true,
     express: app
 });
+
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.send('first app');
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
 
 router.get('/', function (req, res) {
     res.send('admin app');
 });
 
-router.get('/products', function (req, res) {
-    res.render('admin/products.html', {'school': 'nodejs'});
+router.get( '/products' , function( _ ,res){
+    models.Products.findAll({
+
+    }).then( (products) => {
+        // DB에서 받은 products를 products변수명으로 내보냄
+        res.render( 'admin/products.html' ,{ products : products });
+    });
+});
+
+router.get('/products/write', function (req,res){
+    res.render( 'admin/form.html');
+});
+
+router.post('/products/write' , (req,res) => {
+    models.Products.create({
+        name : req.body.name,
+        price : req.body.price ,
+        description : req.body.description
+    }).then( () => {
+        res.redirect('/admin/products');
+    });
 });
 
 module.exports = router;
