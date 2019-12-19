b51547e319571d9097b43af1948e514179f1abaa
wonwooCho <jwwjjang@gmail.com>
2019-12-19 22:39:51 +0900

    add product detail page.

--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,23 @@ router.get('/', function (req, res) {
     res.send('admin app');
 });
 
-router.get( '/products' , function( _ ,res){
+router.get('/products' , function( _ ,res){
     models.Products.findAll({
 
     }).then( (products) => {
         // DB에서 받은 products를 products변수명으로 내보냄
-        res.render( 'admin/products.html' ,{ products : products });
+        res.render( 'admin/products.html' , { products : products });
     });
 });
 
 router.get('/products/write', function (req,res){
     res.render( 'admin/form.html');
+});
+
+router.get('/products/detail/:id' , function(req, res){
+    models.Products.findByPk(req.params.id).then( (product) => {
+        res.render('admin/detail.html', { product: product });  
+    });
 });
 
 router.post('/products/write' , (req,res) => {
