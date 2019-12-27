71106bf4075547ff2637bdd5730b1d08d1a15b9d
wonwooCho <jwwjjang@gmail.com>
2019-12-27 16:24:44 +0900

    remove *.envcpy from .gitignore

--- routes/admin.js
+++ routes/admin.js
@@ -6,31 +6,30 @@ router.get('/', function (req, res) {
     res.send('admin app');
 });
 
-router.get('/products' , function( _ ,res){
+router.get('/products' , function(_, res) {
     models.Products.findAll({
 
-    }).then( (products) => {
-        // DB에서 받은 products를 products변수명으로 내보냄
-        res.render( 'admin/products.html' , { products : products });
+    }).then((data_from_db) => {
+        res.render('admin/products.html', { products : data_from_db });
     });
 });
 
-router.get('/products/write', function (req,res){
-    res.render( 'admin/form.html');
+router.get('/products/write', function (req, res) {
+    res.render('admin/form.html');
 });
 
-router.get('/products/detail/:id' , function(req, res){
-    models.Products.findByPk(req.params.id).then( (product) => {
+router.get('/products/detail/:id' , function(req, res) {
+    models.Products.findByPk(req.params.id).then((product) => {
         res.render('admin/detail.html', { product: product });  
     });
 });
 
-router.post('/products/write' , (req,res) => {
+router.post('/products/write' , (req, res) => {
     models.Products.create({
         name : req.body.name,
         price : req.body.price ,
         description : req.body.description
-    }).then( () => {
+    }).then(() => {
         res.redirect('/admin/products');
     });
 });
