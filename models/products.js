2d7c436b070c668847c3a2c2d224c15d57eae1aa
wonwooCho <jwwjjang@gmail.com>
2019-12-31 18:26:37 +0900

    add  contacts url that functions the same as the admin url.

--- models/products.js
+++ models/products.js
@@ -1,16 +1,16 @@
 const moment = require('moment');
 
-module.exports = function(sequelize, DataTypes) {
-    var Products = sequelize.define('Products', {
-        id : { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-        name : { type: DataTypes.STRING },
-        price : { type: DataTypes.INTEGER },
-        description : { type: DataTypes.TEXT }
+module.exports = (sequelize, data_types) => {
+    var products = sequelize.define('Products', {
+        id: { type: data_types.INTEGER, primaryKey: true, autoIncrement: true },
+        name: { type: data_types.STRING },
+        price: { type: data_types.INTEGER },
+        description: { type: data_types.TEXT }
     });
 
-    Products.prototype.dateFormat = (date) => (
+    products.prototype.dateFormat = (date) => (
         moment(date).format('YYYY-MM-DD')
     );
 
-    return Products;
+    return products;
 }
