6d6dd4f029ebfa32895df329216774233179301e
wonwooCho <wwoo.cho92@gmail.com>
2020-01-21 22:40:02 +0900

    display user list at chatting page

--- helpers/socketConnection.js
+++ helpers/socketConnection.js
@@ -1,10 +1,20 @@
+require('./removeByBalue')();
+
 module.exports =  (io) => {
+    let userList = []; // 사용자 리스트를 저장할곳
+
     io.on('connection', socket => { 
         // console.log('채팅을 위한 소켓서버 접속완료');
 
         //아래 두줄로 passport의 req.user의 데이터에 접근한다.
         const session = socket.request.session.passport;
         const user = typeof session !== 'undefined' ? session.user : '';
+
+        // userList 필드에 사용자 명이 존재 하지 않으면 삽입
+        if (!userList.includes(user.displayname)) {
+            userList.push(user.displayname);
+        }
+        io.emit('join', userList);
         
         socket.on('client message', data => {
             // console.log(data);
@@ -15,14 +25,14 @@ module.exports =  (io) => {
         });
 
         // 예약어는 connection, disconnect정도. 나머지는 비교적 자유롭게 네이밍 가능
-        socket.on('disconnect', data => {
-            console.log('DISCONNECT', data);
+        socket.on('disconnect', () => {            
+            userList.removeByValue(user.displayname);
+            io.emit('leave', userList);
         });
     });
 };
 
 /*
-
 디비에 저장된 Sessions -> data 내용
 
 {
@@ -43,5 +53,4 @@ module.exports =  (io) => {
         }
     }
 }
-
 */
