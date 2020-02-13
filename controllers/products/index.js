26c655aab87e9d09e83feb13c5148e2bb9edd345
wonwooCho <wwoo.cho92@gmail.com>
2020-02-13 21:51:03 +0900

    /products/detail.html 에서 좋아요 기능 추가

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../../models');
+const loginRequired = require('../../middleware/loginRequired');
 
 router.get('/:id' , async(req, res) => {
     try {
         const product = await models.Products.findByPk(req.params.id);
-        res.render('products/detail.html', { product });  
+        const userLikes = await require('../../helpers/userLikes')(req);
+        res.render('products/detail.html', { product, userLikes });  
     } catch(e) {
-        
+
+    }
+});
+
+router.post('/like/:product_id(\\d+)', loginRequired, async(req, res) => {
+    try {
+        const product = await models.Products.findByPk(req.params.product_id);
+        const user = await models.User.findByPk(req.user.id);
+        const status = await user.addLikes(product);
+
+        res.json({
+            status
+        });
+    } catch(e) {
+        console.log(e);
     }
 });
 
