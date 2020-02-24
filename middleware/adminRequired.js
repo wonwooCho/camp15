a87b275950fed760d1bbb3c723ee29a1ea745e0e
wonwooCho <wwoo.cho92@gmail.com>
2020-02-24 23:57:58 +0900

    어드민 검사 수정

--- middleware/adminRequired.js
+++ middleware/adminRequired.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
     if (!req.isAuthenticated()){ 
         res.redirect('/accounts/login');
     }else{
-        if (req.user.username !== 'admin') {
+        if (req.user.username !== 'yain1468@naver.com') {
             res.send('<script>alert("관리자만 접근 가능합니다.");\location.href="/accounts/login";</script>');
         } else {
             return next();
